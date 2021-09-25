bcfd8ffba411947f95d8a4086537c2bda87abde9
ZBreezie <zpmc55@yahoo.com>
2021-09-24 23:58:40 -0400

    basic quiz functionality

--- script.js
+++ script.js
@@ -1,73 +1,98 @@
 var startButton = document.getElementById('startButton')
 var questionContent = document.getElementById('question-container')
+var currentQuestion = 0
 
+//get all answer buttons by class for answer choice comparison
+var answerButton = document.getElementsByClassName('answerbtn')
 
 
 function startQuiz() {
     console.log('Hello')
+    console.log(startButton.value)
     startButton.classList.add('hide')
     questionContent.classList.remove('hide')
-    loadNextQ()
-}
-
-function loadNextQ() {
+    loadAnswers()
 
 }
 
-function selectAnswer() {
+function loadAnswers() {
+    document.getElementById("question").innerHTML = questionsArray[currentQuestion].question
+
+        for(var i=0; i < 4; i++) {
+        answerButton [i].innerHTML = questionsArray[currentQuestion].choices[i]
+    }
+}
+
+function checkAnswer(clientInput) {
+    console.log(clientInput.currentTarget.value)
+    var userAnswer = clientInput.currentTarget.value
+    
+    if (userAnswer == questionsArray[currentQuestion].correctAnswer){
+        score ++
+        console.log("correctAnswer")
+    }
+    console.log(score)
+    currentQuestion ++
+
+    if (currentQuestion > 4) {
+        questionContent.classList.add('hide')
+    }
+    
+    else {
+        loadAnswers()
+    }
+    
 
 }
 
-startButton.addEventListener('click', startQuiz)
+$('#startButton').on('click', startQuiz); {
 
-var questionsList = [
+   
+};
+
+
+$('.answerbtn').on('click', checkAnswer); {
+   
+};
+
+
+
+var score = 0;
+
+var questionsArray = [
     {
-        question: "Commonly used data types do NOT include",
-        answers: [
-            {text: 'strings', correct: false},
-            {text: 'booleans', correct: false},
-            {text: 'numbers', correct: false},
-            {text: 'alerts', correct: true}
-        ]
+        question: "Commonly used data types do NOT include _______",
+        choices: ['strings', 'booleans', 'numbers', 'alerts'],
+        correctAnswer: 3
     },
 
     {
         question: "Arrays in JavaScript can be used to store _______",
-        answers: [
-            {text: 'numbers and strings', correct: false},
-            {text: 'other arrays', correct: false},
-            {text: 'booleans', correct: false},
-            {text: 'all of the above', correct: true}
-        ]
+        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        correctAnswer: 3
     },
 
     {
         question: "The condition in an if/else statement is enclosed with _______",
-        answers: [
-            {text: 'quotes', correct: false},
-            {text: 'curly brackets', correct: false},
-            {text: 'parentheses', correct: true},
-            {text: 'semicolons', correct: false}
-        ]
+        choices: ['quotes', 'curly brackets', 'parentheses', 'semicolons'],
+        correctAnswer: 2
     },
 
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is ______",
-        answers: [
-            {text: 'for loops', correct: false},
-            {text: 'if/else statements', correct: false},
-            {text: 'console.log', correct: true},
-            {text: 'screwdriver', correct: false}
-        ]
+        choices: ['for loops', 'if/else statements', 'console.log', 'screwdriver'],
+        correctAnswer: 2
     },
 
     {
         question: "String values must be enclosed within _______ before being assigned to variables",
-        answers: [
-            {text: 'commas', correct: false},
-            {text: 'curly brackets', correct: false},
-            {text: 'quotes', correct: true},
-            {text: 'parentheses', correct: false}
-        ]
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        correctAnswer: 2
     }
 ]
+
+// end screen
+//timer
+//output score
+//store score in local
+//
